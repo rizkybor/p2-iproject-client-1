57d180f5f49f3c111c635a917a73ae941e18bc61
rizkybor <rizkyak994@gmail.com>
2021-12-16 12:50:17 +0700

    change server heroku

--- apis/server.js
+++ apis/server.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstances = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://ifootballku.herokuapp.com/"
 })
 
  export default axiosInstances
